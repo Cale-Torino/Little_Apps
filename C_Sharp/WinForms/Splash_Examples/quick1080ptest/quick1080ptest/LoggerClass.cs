af089ea8451b1566909abcba21d018cec57bac39
Cale Torino <52330180+Cale-Torino@users.noreply.github.com>
2021-06-03 11:40:39 -0700

    added splash

---
+++ C_Sharp/WinForms/Splash_Examples/quick1080ptest/quick1080ptest/LoggerClass.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace quick1080ptest
+{
+    class LoggerClass
+    {
+        public static class Logger
+        {
+            //Create logfile log. file
+            private static string LogFile = Application.StartupPath + "\\Logs\\quick1080ptest_" + DateTime.Now.ToString("yyyy-dd-M--HH-mm-ss") + ".log";
+
+            public static void WriteLine(string txt)
+            {
+                try
+                {
+                    //Write to the logfile
+                    File.AppendAllText(LogFile, "[" + DateTime.Now.ToString() + "] : " + txt + "\n");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Could Not Append Text To Log File", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+            }
+
+            public static void DeleteLog()
+            {
+                try
+                {
+                    //Delete the log file
+                    File.Delete(LogFile);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Could Not Delete Log File", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+            }
+        }
+    }
+}
---
+++ C_Sharp/WinForms/Splash_Examples/quick1080ptest/quick1080ptest/SplashForm.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static quick1080ptest.LoggerClass;
+
+namespace quick1080ptest
+{
+    public partial class SplashForm : Form
+    {
+        public SplashForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Ini()
+        {
+            try
+            {
+                //Initiate the app by calling these methods
+                CreateFolder();
+                Sleepp();
+                OpenForm();
+                Logger.WriteLine(" *** Ini Complete [SplashForm] ***");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ini Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Logger.WriteLine(" *** Error:" + ex.Message + " [SplashForm] ***");
+                return;
+            }
+        }
+
+        private async void Sleepp()
+        {
+            try
+            {
+                //Create the folders used by the app
+                Logger.WriteLine(" *** Sleeping Start [SplashForm] ***");
+                await Task.Delay(5000);
+                //Thread.Sleep(5000);
+                Logger.WriteLine(" *** Sleeping End [SplashForm] ***");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Create Folder Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Logger.WriteLine(" *** Error:" + ex.Message + " [SplashForm] ***");
+                return;
+            }
+        }
+
+        private void CreateFolder()
+        {
+            try
+            {
+                //Create the folders used by the app
+                string path = Application.StartupPath;
+                Directory.CreateDirectory(path + "\\Logs");
+                Logger.WriteLine(" *** Application Start [SplashForm] ***");
+                Logger.WriteLine(" *** CreateDirectory Success [SplashForm] ***");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Create Folder Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Logger.WriteLine(" *** Error:" + ex.Message + " [SplashForm] ***");
+                return;
+            }
+        }
+
+        private void OpenForm()
+        {
+            try
+            {
+                //Open the Main Form
+                Logger.WriteLine(" *** Open Main Form [SplashForm] ***");
+                Form f = new MainForm();
+                //Hide();
+                f.ShowDialog();
+                Close();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Open Main Form Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Logger.WriteLine(" *** Error:" + ex.Message + " [SplashForm] ***");
+                return;
+            }
+        }
+
+        private void SplashForm_Load(object sender, EventArgs e)
+        {
+            Ini();
+        }
+    }
+}
