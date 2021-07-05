0fcfd943a1c111fa41b2b2a5598b2b13065ec0a6
Cale Torino <cale@techrad.co.za>
2021-07-05 12:23:10 +0200

    Windows service console app added

---
+++ C_Sharp/CLI/Windows_Service/ServiceConsoleApp/README.md
@@ -0,0 +1,16 @@
+
+
+open cmd as administrator in directory then follow the instructions bellow:
+
+1. create service in cmd
+
+```
+SC CREATE "WindowsService1" binpath= "C:\Users\UserName\source\repos\WindowsService1\WindowsService1\bin\Release\WindowsService1.exe" DisplayName= "WindowsService1" start=auto
+```
+
+2. delete service in cmd
+
+```
+SC DELETE WindowsService1
+```
+
---
+++ C_Sharp/CLI/Windows_Service/ServiceConsoleApp/ServiceConsoleApp/ServiceClass.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace ServiceConsoleApp
+{
+    class ServiceClass
+    {
+        private readonly Timer _timer;
+
+        public ServiceClass()
+        {
+            _timer = new Timer(1000) { AutoReset = true };
+            _timer.Elapsed += TimerElapsed;
+        }
+
+        private void TimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            string[] lines = new string[] { DateTime.Now.ToString() };
+            File.AppendAllLines(@"ServiceClass.log", lines);
+        }
+
+        public void Start()
+        {
+            _timer.Start();        
+        }
+
+        public void Stop()
+        {
+            _timer.Stop();
+        }
+
+    }
+}
