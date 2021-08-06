52340fd3d465c44be0b553bbcc7e4fb632d0d70c
Cale Torino <52330180+Cale-Torino@users.noreply.github.com>
2021-08-06 02:39:23 -0700

    added simple TCP example

---
+++ 1. C_Sharp/3. WinForms/6. SuperSimpleTCP/SuperSimpleTCP/MainForm.Designer.cs
@@ -0,0 +1,215 @@
+﻿
+namespace SuperSimpleTCP
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.Disconnectbutton = new System.Windows.Forms.Button();
+            this.MSGcomboBox = new System.Windows.Forms.ComboBox();
+            this.Sendbutton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Connectbutton = new System.Windows.Forms.Button();
+            this.IPtextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.LogstextBox = new System.Windows.Forms.TextBox();
+            this.listBox = new System.Windows.Forms.ListBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.Disconnectbutton);
+            this.groupBox1.Controls.Add(this.MSGcomboBox);
+            this.groupBox1.Controls.Add(this.Sendbutton);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.Connectbutton);
+            this.groupBox1.Controls.Add(this.IPtextBox);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(157, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(678, 193);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Controls";
+            // 
+            // Disconnectbutton
+            // 
+            this.Disconnectbutton.Location = new System.Drawing.Point(226, 98);
+            this.Disconnectbutton.Name = "Disconnectbutton";
+            this.Disconnectbutton.Size = new System.Drawing.Size(75, 23);
+            this.Disconnectbutton.TabIndex = 8;
+            this.Disconnectbutton.Text = "Disconnect";
+            this.Disconnectbutton.UseVisualStyleBackColor = true;
+            this.Disconnectbutton.Click += new System.EventHandler(this.Disconnectbutton_Click);
+            // 
+            // MSGcomboBox
+            // 
+            this.MSGcomboBox.FormattingEnabled = true;
+            this.MSGcomboBox.Items.AddRange(new object[] {
+            "callGPIO",
+            "callRESTART"});
+            this.MSGcomboBox.Location = new System.Drawing.Point(9, 71);
+            this.MSGcomboBox.Name = "MSGcomboBox";
+            this.MSGcomboBox.Size = new System.Drawing.Size(292, 21);
+            this.MSGcomboBox.TabIndex = 7;
+            // 
+            // Sendbutton
+            // 
+            this.Sendbutton.Location = new System.Drawing.Point(64, 98);
+            this.Sendbutton.Name = "Sendbutton";
+            this.Sendbutton.Size = new System.Drawing.Size(75, 23);
+            this.Sendbutton.TabIndex = 5;
+            this.Sendbutton.Text = "Send";
+            this.Sendbutton.UseVisualStyleBackColor = true;
+            this.Sendbutton.Click += new System.EventHandler(this.Sendbutton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Message";
+            // 
+            // Connectbutton
+            // 
+            this.Connectbutton.Location = new System.Drawing.Point(145, 98);
+            this.Connectbutton.Name = "Connectbutton";
+            this.Connectbutton.Size = new System.Drawing.Size(75, 23);
+            this.Connectbutton.TabIndex = 2;
+            this.Connectbutton.Text = "Connect";
+            this.Connectbutton.UseVisualStyleBackColor = true;
+            this.Connectbutton.Click += new System.EventHandler(this.Connectbutton_Click);
+            // 
+            // IPtextBox
+            // 
+            this.IPtextBox.Location = new System.Drawing.Point(9, 32);
+            this.IPtextBox.Name = "IPtextBox";
+            this.IPtextBox.Size = new System.Drawing.Size(292, 20);
+            this.IPtextBox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "IP";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.LogstextBox);
+            this.groupBox2.Location = new System.Drawing.Point(157, 211);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(678, 187);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Logs";
+            // 
+            // LogstextBox
+            // 
+            this.LogstextBox.BackColor = System.Drawing.Color.Black;
+            this.LogstextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.LogstextBox.ForeColor = System.Drawing.Color.White;
+            this.LogstextBox.HideSelection = false;
+            this.LogstextBox.Location = new System.Drawing.Point(3, 16);
+            this.LogstextBox.Multiline = true;
+            this.LogstextBox.Name = "LogstextBox";
+            this.LogstextBox.ReadOnly = true;
+            this.LogstextBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.LogstextBox.Size = new System.Drawing.Size(672, 168);
+            this.LogstextBox.TabIndex = 0;
+            // 
+            // listBox
+            // 
+            this.listBox.BackColor = System.Drawing.Color.Black;
+            this.listBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox.ForeColor = System.Drawing.Color.White;
+            this.listBox.FormattingEnabled = true;
+            this.listBox.Location = new System.Drawing.Point(3, 16);
+            this.listBox.Name = "listBox";
+            this.listBox.Size = new System.Drawing.Size(136, 367);
+            this.listBox.TabIndex = 6;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.listBox);
+            this.groupBox3.Location = new System.Drawing.Point(12, 12);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(142, 386);
+            this.groupBox3.TabIndex = 7;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Connected Clients";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(847, 410);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SuperSimple TCP";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button Sendbutton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button Connectbutton;
+        private System.Windows.Forms.TextBox IPtextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox LogstextBox;
+        private System.Windows.Forms.ListBox listBox;
+        private System.Windows.Forms.ComboBox MSGcomboBox;
+        private System.Windows.Forms.Button Disconnectbutton;
+        private System.Windows.Forms.GroupBox groupBox3;
+    }
+}
+
