2ff69f584fc403a326b406ed4833b13edd99a731
Cale Torino <cale@techrad.co.za>
2021-08-16 12:51:43 +0200

    Logs4Net Example

---
+++ 1. C_Sharp/3. WinForms/8. Logs4NetExample/Logs4NetExample/MainForm.Designer.cs
@@ -0,0 +1,110 @@
+﻿
+namespace Logs4NetExample
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.StatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(212, 42);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "First test";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.MouseEnter += new System.EventHandler(this.button1_MouseEnter);
+            this.button1.MouseLeave += new System.EventHandler(this.button1_MouseLeave);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(264, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(212, 42);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Second Test";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.button2.MouseEnter += new System.EventHandler(this.button2_MouseEnter);
+            this.button2.MouseLeave += new System.EventHandler(this.button2_MouseLeave);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.StatusLabel});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 126);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(488, 32);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // StatusLabel
+            // 
+            this.StatusLabel.Name = "StatusLabel";
+            this.StatusLabel.Size = new System.Drawing.Size(60, 25);
+            this.StatusLabel.Text = "Ready";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(488, 158);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MainForm";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel StatusLabel;
+    }
+}
+
---
+++ 1. C_Sharp/3. WinForms/8. Logs4NetExample/Logs4NetExample/MainForm.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Logs4NetExample
+{
+    public partial class MainForm : Form
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            log.Info("**APPLICATION START**");
+            //log.Debug("Hello Debug world!");
+            //log.Error("Hello Error world!");
+            //log.Fatal("Hello Fatal world!");
+            //log.Warn("Hello Warn world!");
+            StatusLabel.Text = "Ready";
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            log.Info("** Button1 Clicked **");
+            StatusLabel.Text = $"[{DateTime.Now}] Button1 Clicked";
+        }
+
+        private void button1_MouseEnter(object sender, EventArgs e)
+        {
+            log.Info("** Button1 MouseEnter **");
+            StatusLabel.Text = $"[{DateTime.Now}] Button1 MouseEnter";
+        }
+
+        private void button1_MouseLeave(object sender, EventArgs e)
+        {
+            log.Info("** Button1 MouseLeave **");
+            StatusLabel.Text = $"[{DateTime.Now}] Button1 MouseLeave";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            log.Info("** Button2 Clicked **");
+            StatusLabel.Text = $"[{DateTime.Now}] Button2 Clicked";
+        }
+
+        private void button2_MouseEnter(object sender, EventArgs e)
+        {
+            log.Info("** Button2 MouseEnter **");
+            StatusLabel.Text = $"[{DateTime.Now}] Button2 MouseEnter";
+        }
+
+        private void button2_MouseLeave(object sender, EventArgs e)
+        {
+            log.Info("** Button2 MouseLeave **");
+            StatusLabel.Text = $"[{DateTime.Now}] Button2 MouseLeave";
+        }
+    }
+}
