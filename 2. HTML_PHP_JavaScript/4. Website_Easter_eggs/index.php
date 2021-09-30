7d074ee3a32a3242850d0a8040a75eb2097c505f
Cale Torino <cale@techrad.co.za>
2021-09-30 16:09:28 +0200

    Easter eggs

---
+++ 2. HTML_PHP_JavaScript/4. Website_Easter_eggs/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Easter Egg</title>
+<!-- Optional theme -->
+<link rel="stylesheet" href="style.css">
+
+<!-- Jquery -->
+<script src="jquery.min.js"></script>
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="bootstrap.min.js"></script>
+</head>
+<body>
+
+<h1>Easter Egg</h1>
+<p>An example of Easter Eggs for your website.</p>
+<p>
+    <a href="javascript:var%20s%20=%20document.createElement('script');s.type='text/javascript';document.body.appendChild(s);s.src='http://localhost/eastereggs/extend/asteroids.min.js';void(0);" title="Spaceship">Spaceship</a>
+    <a href="javascript:var%20s%20=%20document.createElement('script');s.type='text/javascript';document.body.appendChild(s);s.src='http://localhost/eastereggs/extend/bug.js';void(0);" title="bug">bug</a>
+    <a href="javascript:var%20s%20=%20document.createElement('script');s.type='text/javascript';document.body.appendChild(s);s.src='http://localhost/eastereggs/extend/nyancat.js';void(0);" title="nyancat">nyancat</a> 
+</p>
+</body>
+</html>
