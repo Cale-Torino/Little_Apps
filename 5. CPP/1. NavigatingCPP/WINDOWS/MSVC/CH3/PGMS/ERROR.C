d44ec242bea3bf45eb4f36e14596bca4c6096a7d
Cale-Torino <cale@techrad.co.za>
2022-01-11 23:08:01 +0200

    removed

--- 5. CPP/1. NavigatingCPP/WINDOWS/MSVC/CH3/PGMS/ERROR.C
+++
@@ -1,9 +0,0 @@
-// error.C - default arguments
-#include <iostream.h>
-
-void error(const char *msg, int dlevel)
-{
-	extern int Debug;
-	if (dlevel == 0 || Debug >= dlevel)
-		cerr << msg << endl;
-}
