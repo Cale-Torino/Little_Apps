4eae5a87abb08a8804a058d9efa6bee7af6f36b9
Cale-Torino <cale@techrad.co.za>
2022-02-21 13:38:58 +0200

    Added MergingAssemblies example application

---
+++ 1. C_Sharp/3. WinForms/34. MergingAssemblies/MergingAssemblies/ClassLibrary2/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary2
+{
+    public class Class1
+    {
+        public static string Hello()
+        {
+            return "Hello, this is ClassLibrary2.class1 .dll file method calling";
+        }
+    }
+}
---
+++ 1. C_Sharp/3. WinForms/34. MergingAssemblies/MergingAssemblies/MergingAssemblies/MainForm.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MergingAssemblies
+{
+    public partial class MainForm : Form
+    {
+        //https://github.com/ravibpatel/ILRepack.Lib.MSBuild.Task
+
+        //https://www.meziantou.net/merging-assemblies-using-ilrepack.htm
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            toolStripStatusLabel1.Text = "Ready";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string C1 = ClassLibrary1.Class1.Hello();
+            string C2 = ClassLibrary2.Class1.Hello();
+            toolStripStatusLabel1.Text = $"[{DateTime.Now}] : {C1} | {C2}";
+        }
+    }
+}
