c9efef6adc86913ab3c2687f97cb75bc64c495a3
Cale Torino <52330180+Cale-Torino@users.noreply.github.com>
2022-03-16 03:25:28 -0700

    Added .NetCore5 example

---
+++ 1. C_Sharp/1. CLI/4. ConsoleAppTemplate/ConsoleApp_NetCore/ConsoleApp_NetCore/Classes/LogClass.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_NetCore
+{
+    public static class LogClass
+    {
+        private static readonly string LogFile = AppDomain.CurrentDomain.BaseDirectory + @"\Logs\" + Assembly.GetExecutingAssembly().GetName().Name + "_" + DateTime.Now.ToString("yyyy-dd-M--HH-mm-ss") + ".log";
+
+        public static void WriteLine(string txt)
+        {
+            try
+            {
+                File.AppendAllText(LogFile, "[" + DateTime.Now.ToString() + "] : " + txt + "\n");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                return;
+            }
+        }
+
+        public static void DeleteLog()
+        {
+            try
+            {
+                File.Delete(LogFile);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                return;
+            }
+        }
+    }
+}
---
+++ 1. C_Sharp/1. CLI/4. ConsoleAppTemplate/ConsoleApp_NetCore/ConsoleApp_NetCore/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp_NetCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Directory.CreateDirectory(AppDomain.CurrentDomain.BaseDirectory + @"\Logs");
+            LogClass.WriteLine("Application Start");
+            ComputerClass.GetData();
+            Console.WriteLine($"parameter count = {args.Length}");
+            for (int i = 0; i < args.Length; i++)
+            {
+                Console.WriteLine($"Arg[{i}] = [{args[i]}]");
+            }
+            Console.ReadKey();
+        }
+    }
+}
