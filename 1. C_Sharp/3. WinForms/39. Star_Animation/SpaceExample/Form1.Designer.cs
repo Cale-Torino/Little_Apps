e7ce02889a8241dec007637de1022b1362fb3876
Cale-Torino <cale@techrad.co.za>
2022-04-04 14:21:40 +0200

    Added SpaceSky Moving example.

---
+++ 1. C_Sharp/3. WinForms/39. Star_Animation/SpaceExample/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace SpaceExample
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 50;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(800, 450);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.BackColor = System.Drawing.Color.Transparent;
+            this.label.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.label.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label.Location = new System.Drawing.Point(0, 430);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(55, 20);
+            this.label.TabIndex = 1;
+            this.label.Text = "Ready";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.pictureBox1);
+            this.ForeColor = System.Drawing.Color.Lime;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+    }
+}
+
---
+++ 1. C_Sharp/3. WinForms/39. Star_Animation/SpaceExample/Form1.cs
@@ -0,0 +1,56 @@
+﻿using SpaceStars;
+using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+namespace SpaceExample
+{
+    public partial class MainForm : Form
+    {
+        //the graphics object
+        Graphics _p = new PictureBox().CreateGraphics();
+        //represents the starfield object
+        StarField _s;
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            //Console.Beep();
+            Init();
+            label.Parent = pictureBox1;
+            label.BackColor = Color.Transparent;
+            label.Text = $"Version: {_s.Elac_Version()}";
+            timer1.Start();
+        }
+        public void EnableDoubleBuffering()
+        {
+            SetStyle(ControlStyles.OptimizedDoubleBuffer | ControlStyles.AllPaintingInWmPaint | ControlStyles.UserPaint, true);
+            UpdateStyles();
+        }
+        private void Init()
+        {
+            //use double buffer for smooth animation
+            EnableDoubleBuffering();
+            BackColor = Color.Black;
+            _s = new StarField(Width, Height, 300);
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //move the stars 
+            _s.MoveStars(2, 0, 0);
+            //invalidate AFTER all the movement has been done.  
+            pictureBox1.Invalidate();
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            _p = e.Graphics;
+            _p.SmoothingMode = SmoothingMode.AntiAlias;
+            _s.DrawStars(_p);
+        }
+    }
+}
