5b87823de532e8106daa29e55a8115912c28f6f8
Cale Torino <52330180+Cale-Torino@users.noreply.github.com>
2022-04-06 04:36:10 -0700

    Added Steganography examples

---
+++ 1. C_Sharp/3. WinForms/41. Steganography/Steganography/Steganography/MainForm.Designer.cs
@@ -0,0 +1,79 @@
+﻿
+namespace Steganography
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Mergebutton = new System.Windows.Forms.Button();
+            this.Extractbutton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Mergebutton
+            // 
+            this.Mergebutton.Location = new System.Drawing.Point(12, 12);
+            this.Mergebutton.Name = "Mergebutton";
+            this.Mergebutton.Size = new System.Drawing.Size(75, 23);
+            this.Mergebutton.TabIndex = 0;
+            this.Mergebutton.Text = "Merge";
+            this.Mergebutton.UseVisualStyleBackColor = true;
+            this.Mergebutton.Click += new System.EventHandler(this.Mergebutton_Click);
+            // 
+            // Extractbutton
+            // 
+            this.Extractbutton.Location = new System.Drawing.Point(93, 12);
+            this.Extractbutton.Name = "Extractbutton";
+            this.Extractbutton.Size = new System.Drawing.Size(75, 23);
+            this.Extractbutton.TabIndex = 1;
+            this.Extractbutton.Text = "Extract";
+            this.Extractbutton.UseVisualStyleBackColor = true;
+            this.Extractbutton.Click += new System.EventHandler(this.Extractbutton_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(458, 394);
+            this.Controls.Add(this.Extractbutton);
+            this.Controls.Add(this.Mergebutton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Mergebutton;
+        private System.Windows.Forms.Button Extractbutton;
+    }
+}
+
---
+++ 1. C_Sharp/3. WinForms/41. Steganography/Steganography/Steganography/MainForm.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Steganography
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            string sourceDir = $"{AppDomain.CurrentDomain.BaseDirectory}\\app";
+            string destDir = $"{AppDomain.CurrentDomain.BaseDirectory}\\done";
+            Directory.CreateDirectory(sourceDir);
+            Directory.CreateDirectory(destDir);
+        }
+
+        public int CMDStartServer(string CMD_command, string CMD_workingdir)
+        {
+            try
+            {
+                var p = new ProcessStartInfo();
+                p.UseShellExecute = true;//use the OS shell
+                p.WorkingDirectory = CMD_workingdir;//C:\Windows\System32
+                p.FileName = @"C:\Windows\System32\cmd.exe";//find cmd location
+                //p.Verb = "runas";//run as admin
+                p.Arguments = CMD_command;//set your cmd command
+                p.WindowStyle = ProcessWindowStyle.Normal;//show the window `Normal` or `Hidden` to hide.
+                Process _Process = Process.Start(p);//Initiate the process
+                return _Process.Id;//Return PID
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Could not run command successfully", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return 1;
+            }
+        }
+
+        private void Mergebutton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string sourceDir = $"{AppDomain.CurrentDomain.BaseDirectory}\\app";
+                string destDir = $"{AppDomain.CurrentDomain.BaseDirectory}\\done";
+                if (!File.Exists("foldername.zip"))
+                {
+                    ZipFile.CreateFromDirectory(sourceDir, "foldername.zip");
+                }
+
+                //CMDStartServer($"/k ipconfig", @"C:\Users\User48\source\repos\Steganography\Steganography\bin\Debug");
+                CMDStartServer($"/k copy /b picture.png + foldername.zip outputfilename.jpg", AppDomain.CurrentDomain.BaseDirectory);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+                return;
+            }
+        }
+
+        private void Extractbutton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string sourceDir = $"{AppDomain.CurrentDomain.BaseDirectory}";
+                string destDir = $"{AppDomain.CurrentDomain.BaseDirectory}done";
+                string fName = "picture.png";
+                CMDStartServer($"/k ren outputfilename.jpg outputfilename.zip", AppDomain.CurrentDomain.BaseDirectory);
+                ZipFile.ExtractToDirectory(sourceDir + fName, destDir);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+                return;
+            }
+        }
+    }
+}
